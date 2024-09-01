326b3e39744a360b7c9afd2a224981648a6718c2
amekusa <hello@amekusa.com>
2024-09-01 22:55:24 +0900

    update

--- yt-cringe-remover.js
+++ yt-cringe-remover.js
@@ -5,13 +5,14 @@
 // @version      0.1.0
 // @description  Remove cringe videos & channels on YouTube
 // @match        https://www.youtube.com/*
-// @run-at       document-end
+// @run-at       document-start
 // @grant        none
 // @license      MIT
 // @homepage     https://github.com/amekusa/monkeyscripts
 // ==/UserScript==
 
 (function (doc) {
+	let debug = true ? console.debug : (() => {});
 	let NS = '--ns' + Math.floor(Math.random() * 10000); // namespace
 
 	let filter = [
@@ -21,41 +22,49 @@
 		/(?:コメ付|ゆっくり|ずんだも|(?:2|5|２|５)ちゃんねる)/u,
 	];
 
-	let brand = `${NS}-cursed`;
+	let purged = `${NS}-purged`;
 
 	let purge = item => {
-		// console.debug('purge:', item);
-		item.classList.add(brand);
+		item.classList.add(purged);
+	};
+
+	let check = item => {
+		let info = item.innerText;
+		for (let i = 0; i < filter.length; i++) {
+			if (info.match(filter[i])) {
+				debug('purge:', info, item);
+				purge(item);
+				return;
+			}
+		}
+	};
+
+	let checkAll = sel => {
+		doc.querySelectorAll(`${sel}:not(.${purged})`).forEach(check);
 	};
 
 	let update = () => {
-		check('ytd-video-renderer');
-		check('ytd-rich-item-renderer');
-		check('ytd-reel-item-renderer'); // videos in a carousel list
-		check('ytd-compact-video-renderer'); // videos on the right sidebar
+		checkAll('ytd-video-renderer');
+		checkAll('ytd-rich-item-renderer');
+		checkAll('ytd-reel-item-renderer'); // videos in a carousel list
+		checkAll('ytd-compact-video-renderer'); // videos on the right sidebar
 	};
 
-	let check = sel => {
-		doc.querySelectorAll(`${sel}:not(.${brand})`).forEach(item => {
-			let info = item.innerText;
-			for (let i = 0; i < filter.length; i++) {
-				if (info.match(filter[i])) {
-					purge(item);
-					return;
-				}
-			}
-		});
-	};
-
-	doc.addEventListener('DOMContentLoaded', update);
 	doc.addEventListener('scrollend', update);
-
-	let style = doc.createElement('style');
-	style.innerHTML = `
-.${brand} {
-	opacity: .1 !important;
-}`
-	doc.head.appendChild(style);
+	doc.addEventListener('DOMContentLoaded', () => {
+		let style = doc.createElement('style');
+		style.innerHTML = `
+			.${purged} {
+				opacity: .25 !important;
+			}
+			.${purged} img {
+				opacity: .25 !important;
+				filter: blur(4px) !important;
+			}
+		`;
+		doc.head.appendChild(style);
+		update();
+	});
 
 })(document);
 
