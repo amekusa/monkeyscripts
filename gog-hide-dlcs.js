9f28352bd032d605eb8d8d08c96a527aa98f60d2
amekusa <hello@amekusa.com>
2024-09-01 12:50:26 +0900

    update

--- gog-hide-dlcs.js
+++ gog-hide-dlcs.js
@@ -2,7 +2,7 @@
 // @name         Hide DLCs by default
 // @namespace    amekusa.gog-hide-dlcs
 // @author       amekusa
-// @version      1.0
+// @version      1.0.1
 // @description  Hide DLCs in the store page by default. Doesn't work on some links
 // @match        https://www.gog.com/*
 // @run-at       document-start
@@ -12,7 +12,7 @@
 // ==/UserScript==
 
 (function (doc) {
-	// URL params to add to the store links
+	// url params to add to the store links
 	let params = 'hideDLCs=true';
 	// params += '&discounted=true'; // option: Show only discounted
 	// params += '&hideOwned=true';  // option: Hide all owned products
@@ -27,6 +27,7 @@
 		links.push(doc.querySelectorAll('a[href$="/games"]' + exclude));
 		links.push(doc.querySelectorAll('a[href*="/games?"]' + exclude));
 		links.push(doc.querySelectorAll('a[href*="/games/"]' + exclude));
+		// links.push(doc.querySelectorAll('a[href*="/promo/"]' + exclude));
 		for (let i = 0; i < links.length; i++) links[i].forEach(addParams);
 	};
 	doc.addEventListener('DOMContentLoaded', update);
