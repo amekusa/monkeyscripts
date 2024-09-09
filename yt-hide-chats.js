dc30cf61fa84b04bde4441e2c105de07b24575a7
amekusa <hello@amekusa.com>
2024-09-09 14:49:03 +0900

    [yt] overwhole

--- yt-hide-chats.js
+++ yt-hide-chats.js
@@ -13,65 +13,47 @@
 
 (function (doc) {
 	// --- config ---
-	let interval = 1000;
+	let interval = 1500;
 	let debug = false ? console.debug : (() => {});
 	// --------------
 
-	let states = {
-		FIND:   1,
-		ENSURE: 2,
-		DONE:   3,
-	};
-	let state = states.FIND;
+	let hide = true;
 	let url = window.location.href;
-
-	let setState = x => {
-		state = states[x];
-		debug('state:', x);
-	};
 
 	let find = () => {
 		try {
 			let frame = doc.querySelector('iframe#chatframe');
-			if (!frame) return null;
-			if (!frame.contentWindow.document.querySelector('yt-live-chat-renderer #contents')) return null;
-			return frame.contentWindow.document.querySelector('yt-live-chat-renderer #close-button button');
+			return frame ? frame.contentWindow.document.querySelector('yt-live-chat-renderer #close-button button') : null;
 		} catch (e) {
-			return e;
+			return null;
 		}
 	};
 
+	let unhide = () => {
+		debug('unhide');
+		hide = false;
+	};
+
 	let update = () => {
-		let next;
-		switch (state) {
-		case states.FIND: // mission: find & click the close button
-			var btn = find();
-			if (btn) {
-				if (btn instanceof Error) {
-					setState('DONE');
-				} else {
-					debug('click:', btn);
-					btn.dispatchEvent(new Event('click'));
-					setState('ENSURE');
-				}
-			}
-			return;
-		case states.ENSURE: // mission: ensure the close button does not exist anymore
-			var btn = find();
-			if (btn) {
-				setState('FIND'); // the button is stil there. do it again
-				update();
-			} else setState('DONE'); // success
-			return;
-		case states.DONE: // mission: detect url changes
-			if (url != window.location.href) {
-				url = window.location.href;
-				debug('url changed:', url);
-				setState('FIND');
-			}
-			return;
-		default:
-			console.error('Invalid State');
+		// hook "Show chat" button
+		let btn = doc.querySelector('#chat-container #show-hide-button button');
+		if (btn) {
+			btn.removeEventListener('click', unhide);
+			btn.addEventListener('click', unhide);
+		}
+
+		// detect url change
+		if (url != window.location.href) {
+			url = window.location.href;
+			debug('url changed:', url);
+			hide = true;
+		} else if (!hide) return;
+
+		// find & click the close button
+		btn = find();
+		if (btn) {
+			debug('click:', btn);
+			btn.dispatchEvent(new Event('click'));
 		}
 	};
 
