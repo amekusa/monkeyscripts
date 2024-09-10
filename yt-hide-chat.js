3dc53d6947c461544e485c61466455c07f05b09d
amekusa <hello@amekusa.com>
2024-09-10 18:14:59 +0900

    [yt-hide-chat] v1.2.0

--- yt-hide-chat.js
+++ yt-hide-chat.js
@@ -2,7 +2,7 @@
 // @name         YouTube - Hide chat by default
 // @namespace    amekusa.yt-hide-chat
 // @author       amekusa
-// @version      1.1.4
+// @version      1.2.0
 // @description  Hide chat on YouTube live videos by default.
 // @match        https://www.youtube.com/*
 // @run-at       document-start
